03dd3b96bf719c1c910e0d279f74d5c3e5112a3e
pamukcuirem <irempamukcu1@gmail.com>
2022-10-16 22:45:28 +0300

    BIL359

---
+++ app/src/main/java/com/irempamukcu/myfirstapplication/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.irempamukcu.myfirstapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sayhi(view : View){
+        textView.text = "Hello World"
+
+
+    }
+}
